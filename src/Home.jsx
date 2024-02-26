7f94cb2b87f3bfd31da14e06c236480b0508a3eb
GabyAM <gdgabyam@gmail.com>
2024-02-26 14:31:27 -0300

    adds featured posts section to home page
    All the text displayed is placeholder, but i intend to have a way to feature existing posts from the admin dashboard later.

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,6 +11,72 @@ export function Home() {
                     <button>Read</button>
                 </div>
             </div>
+            <main className="flex-col">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <div className="featured-posts flex-col">
+                    <h2 className="title-primary">Featured posts</h2>
+                    <div>
+                        <div className="featured-posts-grid">
+                            <div className="featured-post main-post">
+                                <div className="post-text flex-col">
+                                    <h3 className="title-primary">
+                                        How i made this blog
+                                    </h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+                                </div>
+                                <div className="image-container">
+                                    <img src=""></img>
+                                </div>
+                            </div>
+                            <div className="featured-post">
+                                <div className="post-text flex-col">
+                                    <h3 className="title-primary">
+                                        Post title
+                                    </h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="featured-post">
+                                <div className="post-text flex-col">
+                                    <h3 className="title-primary">
+                                        Post title
+                                    </h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="rounded-button inverted">
+                        See all posts
+                    </button>
+                </div>
+            </main>
         </>
     );
 }
