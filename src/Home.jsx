4ce75343a69db295d6dcb505c585fc2464055a94
GabyAM <gdgabyam@gmail.com>
2024-02-26 07:50:45 -0300

    implements hero on home page
    the hero displays some heading text, a button to go to posts (later) and a link to log in

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,16 @@
 export function Home() {
     return (
+        <>
+            <div className="hero flex-col">
+                <a href="/login">Login</a>
+                <div className="hero-main-content flex-col">
+                    <div className="hero-text flex-col">
+                        <h2>The blog</h2>
+                        <h1 className="title-primary">This is a headline</h1>
+                    </div>
+                    <button>Read</button>
+                </div>
+            </div>
+        </>
     );
 }
