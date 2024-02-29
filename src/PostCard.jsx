e164a64a52e2f9d5683fa35e72699d71ac2c9efe
GabyAM <gdgabyam@gmail.com>
2024-02-29 11:10:16 -0300

    adds links to post card
    now the title and image redirect to the post (not implemented yet), and the author name will redirect to it's profile later

--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function PostCard({ id, title, author, summary }) {
     const [commentCount, setCommentCount] = useState(null);
@@ -19,22 +20,31 @@ export function PostCard({ id, title, author, summary }) {
     }, [id]);
 
     return (
-            <div className="post-card">
-                <div className="post-card-text flex-col">
-                    <div>
+        <div className="post-card">
+            <div className="post-card-text flex-col">
+                <div>
+                    <Link to={`/post/${id}`}>
                         <h2 className="title-primary">{title}</h2>
-                        <span>By {author}</span>
-                    </div>
-                    <p>{summary}</p>
+                    </Link>
                     <span>
-                        {commentCount !== null
-                            ? `${commentCount} comments`
-                            : 'Unable to load comments'}
+                        By{' '}
+                        <a href="#" className="author-link">
+                            {author}
+                        </a>
                     </span>
                 </div>
+                <p>{summary}</p>
+                <span className="comment-count">
+                    {commentCount !== null
+                        ? `${commentCount} comments`
+                        : 'Unable to load comments'}
+                </span>
+            </div>
+            <Link to={`/post/${id}`}>
                 <div className="image-container">
                     <img></img>
                 </div>
-            </div>
+            </Link>
+        </div>
     );
 }
