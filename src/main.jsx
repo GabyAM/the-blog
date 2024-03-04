f95d467d072e95c60d9cb61d4cdd9ad954f95d0d
GabyAM <gdgabyam@gmail.com>
2024-03-04 16:42:37 -0300

    adds theme to skeletons

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Router } from './Router.jsx';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router></Router>
+        <SkeletonTheme highlightColor="#edeeef" baseColor="#C9CCCF">
+            <Router></Router>
+        </SkeletonTheme>
     </React.StrictMode>
 );
