1ce9a33484fd08a3aedd5f015bb2320fe879b942
GabyAM <gdgabyam@gmail.com>
2024-03-07 12:55:34 -0300

    Implements user form tabs
    The tabs redirect the user to the corresponding form without refreshing the page.

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,13 +1,36 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import './styles/login.css';
 
 export function UserForm() {
+    const location = useLocation();
     return (
         <>
             <div className="user-form-layout">
                 <div className="user-form-section">
                     <div className="user-form-container flex-col">
                         <h1 className="title-primary">Welcome</h1>
+                        <nav>
+                            <ul className="method-tabs flex-row">
+                                <li
+                                    className={
+                                        location.pathname === '/login'
+                                            ? 'selected'
+                                            : ''
+                                    }
+                                >
+                                    <Link to="/login">Log in</Link>
+                                </li>
+                                <li
+                                    className={
+                                        location.pathname === '/signup'
+                                            ? 'selected'
+                                            : ''
+                                    }
+                                >
+                                    <Link to="/signup">Sign up</Link>
+                                </li>
+                            </ul>
+                        </nav>
                         <Outlet></Outlet>
                     </div>
                 </div>
