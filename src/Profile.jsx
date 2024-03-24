8f677de06e83ad5e5c9505ad604a3a12227a6147
GabyAM <gdgabyam@gmail.com>
2024-03-24 12:15:57 -0300

    styles profile page

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { Header } from './Header';
 import { useFetchData } from './hooks/useFetchData';
+import './styles/profile.css';
 import { useEffect } from 'react';
 import { EditIcon } from './Icons';
 import { ProfileCardSkeleton } from './ProfileCardSkeleton';
