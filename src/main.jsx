2748c3207a835ecea1b776621a9c309d437664d4
GabyAM <gdgabyam@gmail.com>
2024-04-30 12:43:10 -0300

    installs react hot toast
    i'll be using it in a few occasions such as sending a comment or updating the profile.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,21 @@ import './index.css';
 import { Router } from './Router.jsx';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import { AuthProvider } from './context/auth.jsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <AuthProvider>
             <SkeletonTheme highlightColor="#edeeef" baseColor="#C9CCCF">
+                <Toaster
+                    position="bottom-right"
+                    toastOptions={{
+                        style: {
+                            border: '1px solid var(--main-grey)',
+                            boxShadow: '2px 2px 6px rgba(0,0,0,0.2)'
+                        }
+                    }}
+                ></Toaster>
                 <Router></Router>
             </SkeletonTheme>
         </AuthProvider>
