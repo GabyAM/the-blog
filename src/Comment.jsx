083c2afdf4618d622dadf4c38a340d17f0df7791
GabyAM <gdgabyam@gmail.com>
2024-04-30 08:43:32 -0300

    adds support for deleted comments
    deleted comments are displayed differently

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -13,6 +13,7 @@ export function Comment({ comment }) {
         }
     }, []);
 
+    const isDeleted = comment.user === null;
     return (
         <div
             className={
@@ -20,24 +21,46 @@ export function Comment({ comment }) {
                 (comment.comments.length ? ' parent-comment' : '')
             }
         >
-            <div className={'comment' + (!textHidden ? ' expanded' : '')}>
-                <div className="image-container">
-                    <img src="/avatar.png"></img>
-                </div>
-                <div ref={text} className="text">
-                    <span>{comment.user.name}</span>
-                    <p className={isOverflown && textHidden ? 'overflown' : ''}>
-                        {comment.text}
-                    </p>
-                    {isOverflown && (
-                        <button
-                            className="toggle-comment-length"
-                            onClick={() => setTextHidden(!textHidden)}
-                        >
-                            {textHidden ? 'Show more' : 'Show less '}
-                        </button>
-                    )}
-                </div>
+            <div
+                className={
+                    'comment' +
+                    (!textHidden ? ' expanded' : '') +
+                    (isDeleted ? ' deleted' : '')
+                }
+            >
+                {isDeleted ? (
+                    <span>This comment was removed</span>
+                ) : (
+                    <>
+                        <div className="image-container">
+                            <img
+                                src={
+                                    comment.user.image === '/images/profile.png'
+                                        ? '/profile.png'
+                                        : `http://localhost:3000${comment.user.image}`
+                                }
+                            ></img>
+                        </div>
+                        <div ref={text} className="text">
+                            <span>{comment.user.name}</span>
+                            <p
+                                className={
+                                    isOverflown && textHidden ? 'overflown' : ''
+                                }
+                            >
+                                {comment.text}
+                            </p>
+                            {isOverflown && (
+                                <button
+                                    className="toggle-comment-length"
+                                    onClick={() => setTextHidden(!textHidden)}
+                                >
+                                    {textHidden ? 'Show more' : 'Show less '}
+                                </button>
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
             {comment.comments.map((reply) => (
                 <Comment key={reply._id} comment={reply}></Comment>
