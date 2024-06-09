8a1a7355a13e103ca5a98b58b7e7580c4b9a99a7
GabyAM <gdgabyam@gmail.com>
2024-06-09 09:25:41 -0300

    Implements recent posts on home page
    removes the post placeholders and instead shows the latest 3 posts.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { RecentPosts } from './RecentPosts';
+
 export function Home() {
     return (
         <>
@@ -24,64 +27,7 @@ export function Home() {
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
-                <div className="featured-posts flex-col">
-                    <h2 className="title-primary">Featured posts</h2>
-                    <div>
-                        <div className="featured-posts-grid">
-                            <div className="featured-post main-post">
-                                <div className="post-text flex-col">
-                                    <h3 className="title-primary">
-                                        How i made this blog
-                                    </h3>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris
-                                        nisi ut aliquip ex ea commodo consequat.
-                                    </p>
-                                </div>
-                                <div className="image-container">
-                                    <img src=""></img>
-                                </div>
-                            </div>
-                            <div className="featured-post">
-                                <div className="post-text flex-col">
-                                    <h3 className="title-primary">
-                                        Post title
-                                    </h3>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris
-                                        nisi ut aliquip ex ea commodo consequat.
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="featured-post">
-                                <div className="post-text flex-col">
-                                    <h3 className="title-primary">
-                                        Post title
-                                    </h3>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris
-                                        nisi ut aliquip ex ea commodo consequat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="rounded-button inverted">
-                        See all posts
-                    </button>
-                </div>
+                <RecentPosts></RecentPosts>
             </main>
             <footer>
                 <div className="container">
---
+++ src/RecentPosts.jsx
@@ -0,0 +1,70 @@
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+
+export function RecentPosts() {
+    function fetchRecentPosts() {
+        return fetch('http://localhost:3000/posts?is_published=true&limit=3')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('asnfpasf');
+                }
+                return res.json();
+            })
+            .then((response) => response.results);
+    }
+
+    const {
+        data: posts,
+        isLoading,
+        error
+    } = useQuery({
+        queryKey: ['landing_recent_posts'],
+        queryFn: fetchRecentPosts
+    });
+
+    if (isLoading) {
+        return <p> loading recent posts...</p>;
+    }
+    if (error) {
+        return <p>{error.message}</p>;
+    }
+    return (
+        <div className="recent-posts-section">
+            <h2>Recent posts</h2>
+            <div className="recent-posts">
+                {posts.map((post, index) => (
+                    <React.Fragment key={post._id}>
+                        <div
+                            className={`recent-post variant-${Math.floor(Math.random() * 3) + 1}`}
+                        >
+                            <Link to={`/post/${post._id}`}>
+                                <div className="image-container">
+                                    <img
+                                        src={`http://localhost:3000${post.image}`}
+                                    ></img>
+                                </div>
+                            </Link>
+                            <div className="post-text flex-col">
+                                <Link to={`/post/${post._id}`}>
+                                    <h3 className="title-primary">
+                                        {post.title}
+                                    </h3>
+                                </Link>
+                                <p>{post.summary}</p>
+                            </div>
+                        </div>
+                        {index < posts.length - 1 && (
+                            <div className="horizontal-separator"></div>
+                        )}
+                    </React.Fragment>
+                ))}
+                <Link to={'/posts'} className="posts-link">
+                    <button className="large outlined-button-900 rounded">
+                        See all posts
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+}
